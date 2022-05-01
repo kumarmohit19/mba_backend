b2392efe62b3be20ac6d924af4908b59ce794ed3
Kumar <m.kumarmohit19@gmail.com>
2022-05-01 13:51:19 +0530

    mba initial commit

---
+++ configs/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  DB_NAME: "mba_db",
+  DB_URL: "mongodb://localhost/mba_db",
+  PRODUCTION_DB_URL:
+    "mongodb+srv://admin:Welcome1@movie-booking-app.yjxuy.mongodb.net/movie-booking-app?retryWrites=true&w=majority&ssl=true",
+};
